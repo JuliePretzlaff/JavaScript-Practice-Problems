5e5bad078f97bc91cc94335db2645ab3107929ae
Julie <Juliapretzlaff@gmail.com>
2018-05-03 11:03:55 -0400

    also implemented example solution to problem one

--- JSpractice/JavaScript.js
+++ JSpractice/JavaScript.js
@@ -1,4 +1,5 @@
-﻿$("#caps").click(function () {
+﻿//problem one
+$("#caps").click(function () {
     //alert("hello")
     var input = prompt("Enter a phrase");
     var inputArray = input.split(" ");
@@ -18,3 +19,16 @@
     alert(output);
 
 });
+
+//example one solution
+function uppercase() {
+    str = prompt("input a string");
+    var array1 = str.split(' ');
+    var newarray1 = [];
+
+    for (var x = 0; x < array1.length; x++) {
+        newarray1.push(array1[x].charAt(0).toUpperCase() + array1[x].slice(1));
+    }
+    return newarray1.join(' ');
+}
+console.log(uppercase("the quick brown fox"));
