bfe3679e33bd14b6a1afcc5ddb48cc0d02dd9066
Julie <Juliapretzlaff@gmail.com>
2018-05-03 10:35:07 -0400

    middle of problem 1

---
+++ JSpractice/JavaScript.js
@@ -0,0 +1,14 @@
+﻿$("#caps").click(function () {
+    //alert("hello")
+    var input = prompt("Enter a phrase");
+    var inputArray = input.split(" ");
+
+    alert(inputArray[0]);
+
+    for (var i = 0; i < inputArray.length; i++) {
+        var innerArray = inputArray[i].split("");
+        innerArray[0].toUpperCase();
+        alert(innerArray[0].toUpperCase);
+    }
+
+});
