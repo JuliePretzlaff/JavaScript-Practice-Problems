e3003afff5847ac3cf4a74e4beb73e811ad978ec
Julie <Juliapretzlaff@gmail.com>
2018-05-04 12:16:12 -0400

    added second problem

--- JSpractice/JavaScript.js
+++ JSpractice/JavaScript.js
@@ -1,24 +1,4 @@
-﻿//problem one
-$("#caps").click(function () {
-    //alert("hello")
-    var input = prompt("Enter a phrase");
-    var inputArray = input.split(" ");
-   
-    var output = "";
-
-    for (var i = 0; i < inputArray.length; i++) {
-        var innerArray = inputArray[i].split("");
-        var word = innerArray[0].toUpperCase();
-      
-        for (var ii = 1; ii < innerArray.length; ii++) {
-              //alert(innerArray[ii]);
-              word += innerArray[ii];
-        }
-        output += word + " ";
-    }
-    alert(output);
-
-});
+﻿
 
 //example one solution
 function uppercase() {
@@ -29,6 +9,6 @@ function uppercase() {
     for (var x = 0; x < array1.length; x++) {
         newarray1.push(array1[x].charAt(0).toUpperCase() + array1[x].slice(1));
     }
-    return newarray1.join(' ');
+    alert (newarray1.join(' '));
 }
 console.log(uppercase("the quick brown fox"));
---
+++ JSpractice/jsbody.js
@@ -0,0 +1,34 @@
+﻿//problem one: capitalize first letters of words
+$("#caps").click(function () {
+    //alert("hello")
+    var input = prompt("Enter a phrase");
+    var inputArray = input.split(" ");
+
+    var output = "";
+
+    for (var i = 0; i < inputArray.length; i++) {
+        var innerArray = inputArray[i].split("");
+        var word = innerArray[0].toUpperCase();
+
+        for (var ii = 1; ii < innerArray.length; ii++) {
+            word += innerArray[ii];
+        }
+        output += word + " ";
+    }
+    alert(output);
+
+});
+
+//problem 2: count vowels
+$("#vowels").click(function () {
+    var string = prompt("Enter a phrase.").toLowerCase();
+    var charArray = string.split("");
+    var count = 0;
+
+    for (var i = 0; i < charArray.length; i++) {
+        if (charArray[i] == "a" || charArray[i] == "e" || charArray[i] == "i" || charArray[i] == "o" || charArray[i] == "u") {
+            count++;
+        }
+    }
+    alert(count);
+});
